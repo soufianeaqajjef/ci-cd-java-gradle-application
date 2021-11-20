0336165a397dd54191e08aa05b5727688c7103ae
soufiane aqajjef <soufiane.aqajjef@ibm.com>
2021-11-20 23:54:45 +0100

    sonar test3

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,12 +1,11 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
